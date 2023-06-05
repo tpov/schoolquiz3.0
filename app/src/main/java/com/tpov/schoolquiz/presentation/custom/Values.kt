2875a5a57bded6fc227a2d0af56e25c97c383dc2
tpov <oleg12794@gmail.com>
2023-06-05 20:04:25 +0300

    Create search in quiz5 Create filter start question activity Edit filter search translate question

--- app/src/main/java/com/tpov/schoolquiz/presentation/custom/Values.kt
+++ app/src/main/java/com/tpov/schoolquiz/presentation/custom/Values.kt
@@ -1,4 +1,17 @@
 package com.tpov.schoolquiz.presentation.custom
 
+import java.util.*
+
 object Values {
+    fun getNameThropy(language: String): String {
+        return when (language) {
+            //🥇🥈️🥉🎖🏅🏆🎗️🎃🎄🎁🎧🎞️🪙📀🏆⭐🎯🎓🏰❤
+            "\uD83E\uDD47" -> "занявший 1 место в турнире"
+            "\uD83E\uDD48" -> "Игрок занявший 2 место в турнире"
+            "\uD83E\uDD49" -> "занявший 3 место в турнире"
+            "Немецкий" -> "de"
+            "Испанский" -> "es"
+            else -> Locale.getDefault().language
+        }
+    }
 }
